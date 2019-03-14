a923cfb4d81e95ad49a0e70299dd7ec4083482d2
ashraf <ashraf1abdelrasool@gmail.com>
2019-03-14 10:01:39 +0200

    Initial commit

---
+++ gatewayx/src/main/java/org/revo/stlone/gatewayx/GatewayxApplication.java
@@ -0,0 +1,13 @@
+package org.revo.stlone.gatewayx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayxApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/revo/stlone/StloneApplication.java
@@ -0,0 +1,13 @@
+package org.revo.stlone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StloneApplication.class, args);
+    }
+
+}
